45af46211c6818f53ad7c1ba2845a6ce001e6cbf
MarshmallowSoup <lysetskyyv@gmail.com>
2022-08-02 11:01:45 +0300

    added autosearch of ami

--- terraform/app.tf
+++ terraform/app.tf
@@ -2,15 +2,25 @@ provider "aws" {
     region = "us-east-1"
 }
 
+data "aws_ami" "latest_ubuntu"{
+    owners      = [ "099720109477" ]
+    most_recent = true
+    filter{
+        name  = "name"
+        value = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+}
+
 resource "aws_eip" "static_ip" {
     instance = aws_instance.my_app.id  
 }
 
 resource "aws_instance" "my_app" {
-    ami                    = "ami-052efd3df9dad4825"
+    ami                    =  data.aws_ami.latest_ubuntu.id
     instance_type          = "t2.micro"
     vpc_security_group_ids = [aws_security_group.my_app_SG.id]
     key_name               = "my-key"
+    depends_on = [aws_security_group.my_app_SG]
  
     lifecycle {
        create_before_destroy = true
@@ -45,16 +55,16 @@ resource "aws_security_group" "my_app_SG"{
     }
 
     ingress {
-        from_port = 22
-        to_port   = 22
-        protocol  = "tcp"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
         cidr_blocks = ["${local.ifconfig_co_json.ip}/32"]
     }
 
     egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
@@ -63,8 +73,8 @@ resource "aws_security_group" "my_app_SG"{
 
  resource "local_file" "ansible_inventory" {
   content = templatefile("inventory.tpl",{
-  ip = aws_instance.my_app.public_ip, 
-  path = "/home/softwarenetic/.ssh/my-key.pem"
+  ip      = aws_instance.my_app.public_ip, 
+  path    = "/home/softwarenetic/.ssh/my-key.pem"
  })
   filename = "../ansible/inventory"
 }
